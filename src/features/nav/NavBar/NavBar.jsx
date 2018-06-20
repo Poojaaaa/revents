0c9382e13bf83833f46ecd5c7ab97baa3fc70867
Puja D <me@puja.com>
2018-06-20 12:59:13 -0700

    error in section 15

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { withFirebase } from 'react-redux-firebase'
 import { Menu, Container, Button } from 'semantic-ui-react'
 import { NavLink, Link, withRouter } from 'react-router-dom'
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 import { openModal } from '../../modals/modalActions';
-import { logout } from '../../auth/authActions'
 
 const actions = {
   openModal,
-  logout
+ 
 }
 
 const mapState = (state) => ({
@@ -27,13 +27,13 @@ class NavBar extends Component {
   }
 
   handleSignOut = () => {
-    this.props.logout();
+    this.props.firebase.logout();
     this.props.history.push('/')
   }
 
   render() {
     const {auth} = this.props;
-    const authenticated = auth.authenticated
+    const authenticated = auth.isLoaded && !auth.isEmpty;
 
     return (
     <Menu inverted fixed="top">
@@ -60,7 +60,7 @@ class NavBar extends Component {
         </Menu.Item>}
         
         {authenticated ? (
-          <SignedInMenu currentUser={auth.currentUser} signOut={this.handleSignOut}/> 
+          <SignedInMenu auth={auth} signOut={this.handleSignOut}/> 
         ) : ( 
           <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister}/>
         )}
@@ -70,4 +70,4 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(connect(mapState, actions)(NavBar));
+export default withRouter(withFirebase(connect(mapState, actions)(NavBar)));
