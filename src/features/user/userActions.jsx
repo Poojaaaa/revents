0bf0a9cef998992ee3dc5dcd01d72220f1299761
Puja D <me@puja.com>
2018-06-24 00:14:02 -0700

    error in section 17

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -13,6 +13,47 @@ export const updateProfile = (user) => async (dispatch, getState, {getFirebase})
       toastr.success('Success', 'Profile updated');
     } catch (error) {
       console.log(error);
-      
+    }
+  }
+
+export const uploadProfileImage = (file, fileName) => 
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    const path = `${user.uid}/user_images`;
+    const options = {
+      name: fileName
+    };
+    try {
+      // upload the file to firebase storage
+      let uploadedFile = await firebase.uploadedFile(path, file, null, options);
+      // get url of image
+      let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+      // get userdoc
+      let userDoc = await firestore.get(`users/${user.id}`);
+      // check if user has photo, if not update profile with new Image
+      if (!userDoc.data().photoURL) {
+        await firebase.updateProfile({
+          photoURL: downloadURL
+        });
+        await userDoc.updateProfile({
+          photoURL: downloadURL
+        });
+      }
+      // add the new photo to photos collection
+      return await firestore.add({
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{collection: 'photos'}]
+      }, {
+        name: fileName,
+        url: downloadURL
+      })
+    }
+
+    catch (error) {
+      console.log(error);    
+      throw new Error('Problem uploading photos');
     }
   }
